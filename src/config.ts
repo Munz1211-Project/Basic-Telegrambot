172fb5b79e4670407bc2849e24c99c61e4f9dd85
(x _ x) <64169740+Munz1211@users.noreply.github.com>
2022-11-26 15:19:15 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 //TOKEN
-export const TOKEN: string = "5763835534:AAHGXjNSn0lFKDKkQx4ofXyfxFab0FEIUBk";
+export const TOKEN: string = "TOKEN";
 //ID ADMIN
 export const ID_ADMIN: any = ["1235707578", "5763835534"];
